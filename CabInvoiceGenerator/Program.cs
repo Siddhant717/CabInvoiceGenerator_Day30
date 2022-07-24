03679cc6260b4b20f4a1ed8a69db8c353fb0efd2
Siddhant <santoshshrivastava39@gmail.com>
2022-07-24 16:20:47 +0530

    CabInvoice Generator

--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -11,11 +11,11 @@ namespace CabInvoiceGenerator
             InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
 
             InvoiceGenerator invoice = new InvoiceGenerator(RideType.NORMAL);
-
             double premiumfare = invoiceGenerator.CalculateFare(2.0, 5);
             double normalfare = invoice.CalculateFare(2.0, 5);
             Console.WriteLine("The total fare for Premium Ride is :" + premiumfare);
             Console.WriteLine("The total fare for Normal Ride is :" + normalfare);
+
         }
     }
 }
